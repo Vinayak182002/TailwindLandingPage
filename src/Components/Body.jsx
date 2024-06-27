04ce5685a8f3b9dc3262b665f0466cc802129f71
Shete_Vinayak <shetevinay8314@gmail.com>
2024-06-27 16:20:25 +0530

    landing page completed

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Body = () => {
   return (
-    <div>Body</div>
-  )
-}
+    <div className="space-y-4 lg:flex">
+      <div className="flex items-center justify-center lg:flex-1 lg:order-2 lg:justify-end lg:overflow-hidden">
+        <img
+          src="./assets/Blue-Shape.svg"
+          alt="blue-shape-img"
+          className="-rotate-[45deg] h-64 md:h-72 lg:h-[450px] lg:overflow-hidden"
+        />
+        <img
+          src="./assets/Pink-Shape.svg"
+          alt="pink-shape-img"
+          className="absolute -rotate-[30deg] h-64 md:h-72 lg:h-[450px] lg:overflow-hidden"
+        />
+        <img
+          src="./assets/Purple-Shape.svg"
+          alt="purple-shape-img"
+          className="absolute -rotate-[15deg] h-64 md:h-72 lg:h-[450px] lg:overflow-hidden"
+        />
+        <img
+          src="./assets/Hero-Model.png"
+          alt="hero-img"
+          className="absolute h-64 md:h-72 lg:h-[450px] lg:overflow-hidden"
+/>
+      </div>
 
-export default Body
+      <div className="space-y-4 lg:flex-1 lg:order-1 lg:space-y-10">
+        <h1 className="text-5xl font-bold font-playfair leading-tight">Host your website in less than 5 minutes.</h1>
+        <p className="font-lato text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore velit
+          ratione ducimus ex sapiente placeat quaerat facere vel at sed?
+        </p>
+        <form action="" className="flex flex-col gap-4 md:flex-row">
+          <input className="rounded-md px-4 py-3 placeholder:text-gray-400" type="email" placeholder="Enter your email address" />
+          <button className="rounded-md px-4 py-3 bg-blue-400 text-white hover:bg-blue-600">Joint waitlist now!</button>
+        </form>
+        <div className="flex gap-2 space-y-2">
+          <img src="./assets/Checkmark.svg" alt="checkmark" />
+          <p className="font-lato text-gray-400">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex flex-col gap-12 md:flex-row md:justify-between">
+      <ul className="flex gap-6 font-lato text-gray-400">
+        <li>
+          <a href="#">Facebook</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
+        <li>
+          <a href="#">Instagram</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="flex gap-2">
+        <img src="./assets/Help-Avatar.svg" alt="avatar" />
+        <div>
+          <p className="font-playfair font-thin">Have any questions?</p>
+          <a href="#" className="font-lato font-medium">Talk to our expert</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
